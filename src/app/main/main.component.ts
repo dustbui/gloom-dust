dcda21d3f77ee37e1d85149ddb8261886b78a7c6
dustbui <dustbui@umich.edu>
2019-03-27 15:21:08 -0400

    Implemented delete character

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,46 +4,54 @@ import { Router } from '@angular/router';
 import { Constants } from '../_global/constants';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  public characters: Character[] = [];
-  public windowHeight: number;
-  public containerHeight: number;
-  public createCharacterActions = [
-    { label: 'Get Started', route: 'characterCreation' }
-  ];
+    public characters: Character[] = [];
+    public windowHeight: number;
+    public containerHeight: number;
+    public createCharacterActions = [
+        { label: 'Get Started', route: 'characterCreation' }
+    ];
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.adjustHeights();
-  }
-
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-    this.adjustHeights();
-    const characterStorage = localStorage.getItem('characters');
-    if (characterStorage) {
-      const characterKeys = characterStorage.split(',');
-      characterKeys.forEach(characterKey => {
-        const characterJson = localStorage.getItem(`char:${characterKey}`);
-        if (!characterJson) { return; }
-        const character = JSON.parse(characterJson);
-        this.characters.push(character);
-      });
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.adjustHeights();
     }
-  }
 
-  public createCharacter() {
-    this.router.navigate(['characterCreation']);
-  }
+    constructor(private router: Router) { }
 
-  private adjustHeights() {
-    this.windowHeight = window.innerHeight;
-    this.containerHeight = this.windowHeight - Constants.TOP_BAR_HEIGHT;
-  }
+    ngOnInit() {
+        this.adjustHeights();
+        const characterStorage = localStorage.getItem('characters');
+        if (characterStorage) {
+            const characterKeys = characterStorage.split(',');
+            characterKeys.forEach(characterKey => {
+                const characterJson = localStorage.getItem(`char:${characterKey}`);
+                if (!characterJson) { return; }
+                const character = JSON.parse(characterJson);
+                this.characters.push(character);
+            });
+        }
+    }
+
+    // Event listener from child
+    onCharacterRemove(removedCharacterName: string) {
+        const removedIndex = this.characters.findIndex((character) => {
+            return character.name == removedCharacterName;
+        });
+        this.characters.splice(removedIndex, 1);
+    }
+
+    public createCharacter() {
+        this.router.navigate(['characterCreation']);
+    }
+
+    private adjustHeights() {
+        this.windowHeight = window.innerHeight;
+        this.containerHeight = this.windowHeight - Constants.TOP_BAR_HEIGHT;
+    }
 
 }
--- src/app/main/tile/tile.component.ts
+++ src/app/main/tile/tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Character } from 'src/app/_global/models/character';
 import { TileAction } from 'src/app/_global/models/tileAction';
 
@@ -11,10 +11,32 @@ export class TileComponent implements OnInit {
   @Input() tileName: string;
   @Input() actions: TileAction[];
   @Input() character: Character;
+  @Input() remove: Function;
+  @Output() removedCharacter = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public removeCharacter(characterName: string) {
+    // Remove from characters list
+    let characterStorage = localStorage.getItem('characters');
+    let characterArray = characterStorage.split(',');
+    const characterIndex = characterArray.findIndex((x) => { return x == characterName });
+    characterArray.splice(characterIndex, 1);
+    localStorage.setItem('characters', characterArray.join(','));
+
+    // Remove specific character from local storage
+    localStorage.removeItem(`char:${characterName}`);
+
+    // Emit event to parent to remove from array
+    this.removedCharacter.emit(characterName);
+  }
+
+  public promptRemove(characterName: string) {
+    if (confirm(`Are you sure you want to delete ${characterName}?`)) {
+      this.removeCharacter(characterName);
+    }
+  }
 }
